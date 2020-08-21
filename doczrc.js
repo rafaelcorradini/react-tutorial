a006e4583c61bab8390367d18ffcc94d525018bb
Rafael Corradini da Cunha <rafael.corradini@luizalabs.com>
2020-08-21 16:22:48 -0300

    add starting and components

---
+++ doczrc.js
@@ -0,0 +1,16 @@
+export default {
+  files: 'src/**/*.mdx',
+  menu: [
+    {
+      name: 'Introdução',
+      manue: 'index'
+    },
+    {
+      name: 'Começando',
+      manue: 'starting'
+    }
+  ],
+  themeConfig: {
+    showPlaygroundEditor: true
+  }
+}
